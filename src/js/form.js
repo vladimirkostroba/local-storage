0329dc8bf25f4b1fe18230dbbfc0e5dd26ada98b
Vladimir Kostroba <vladimirkostroba22@gmail.com>
2024-02-03 06:03:00 +0100

    commit

--- src/js/form.js
+++ src/js/form.js
@@ -1,19 +1,52 @@
 const formRef = document.querySelector('.form');
+const textareaRef = document.querySelector('.js-textarea');
+const articlesRef = document.querySelector('.articles');
+console.log( textareaRef);
 
 
-formRef.addEventListener('submit', createArticle);
+formRef.addEventListener('submit', handleFormSubmit);
+textareaRef.addEventListener('input', handleTextAreaInput);
 
 
-function createArticle(e){
-    e.preventDefault();
+populateMassege();
 
-    const article = {
-        titel:e.target.title.value,
-        article:e.target.article.value,
-    }
+function handleFormSubmit(e){
+  e.preventDefault();
 
-    e.target.reset();
+  e.currentTarget.reset();
+  localStorage.removeItem('messege')
+
+  const article = {
+    name:e.target.name.value,
+    feedback:e.target.feedback.value,
+}
+
 
 }
+
+function handleTextAreaInput(e){
+    const messege = e.currentTarget.value;
+    localStorage.setItem('messege',messege);  
+}
+
+function populateMassege(){
+    const savedMassage = localStorage.getItem('messege');
+
+if(savedMassage){
+    textareaRef.value = savedMassage;
+ }
+ }
+
+//  
+
+function createFeedback(obj){
+    return `
+  <li class="feedbacks__item">
+    <h2 class="name">${obj.name}</h2>
+    <p class="text">${obj.feedback}</p>
+  </li>`
+}
+
+
 
 
